3db03605bb5c212e95b5f945c0fdf696fdd6ae83
soyoung-choi <sydesign24@naver.com>
2022-06-22 23:51:31 +0900

    배포 준비

--- backend/app.js
+++ backend/app.js
@@ -3,11 +3,18 @@ var express = require('express')
 var path = require('path')
 var cookieParser = require('cookie-parser')
 var logger = require('morgan')
+const cors = require('cors')
 
 var indexRouter = require('./routes/index')
 var usersRouter = require('./routes/users')
 
 var app = express()
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  })
+)
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'))
@@ -17,7 +24,7 @@ app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
-app.use(express.static('build')) // 서버에서 build 폴더 사용 가능
+app.use(express.static('dist')) // 서버에서 build 폴더 사용 가능
 
 app.use('/', indexRouter)
 app.use('/users', usersRouter)
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router()
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.sendFile(__dirname + '/build/index.html')
+  res.sendFile(__dirname + '/dist/index.html')
 })
 
 module.exports = router
